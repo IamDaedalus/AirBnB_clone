978ac83cc66e212b3072e543d98c969fd57182ea
Daedalus <manny.is.daedalus@pm.me>
2023-10-10 14:25:54 +0000

    removed properties and updated to_dict and cleanup

--- models/base_model.py
+++ models/base_model.py
@@ -19,15 +19,12 @@ class BaseModel:
         This is the constructor for the BaseModel class where
         we initialise some public instance attributes for use
         throughout the program
-
-        Keep everything private to control access rights just
-        like we did in Almost A Circle
         """
         self.id = "{}".format(uuid4())
-        self.__created_at = datetime.now()
+        self.created_at = datetime.now()
         # updated_at is datetime.now() because that was when
         # it was last updated
-        self.__updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """
@@ -41,14 +38,17 @@ class BaseModel:
         This method returns a dictionary representation of the
         class for use in serialization to json objects
         """
-        return {
-                "my_number": self.my_number,
-                "name": self.name,
-                "__class__": type(self).__name__,
-                "updated_at": self.updated_at,
-                "id": self.id,
-                "created_at": self.created_at
-        }
+        my_dict = {}
+
+        # simply iterate through the dictionary and extract the
+        # datetime to convert to isoformat
+        my_dict["__class__"] = "{}".format(type(self).__name__)
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime):
+                my_dict[key] = value.isoformat()
+            else:
+                my_dict[key] = value
+        return my_dict
 
     def _validate_value(self, name, value, Type):
         """
@@ -80,78 +80,3 @@ class BaseModel:
         return "[{:s}] ({:s}) {}".format(
             type(self).__name__, self.id, self.__dict__
             )
-
-    # PUBLIC PROPERTIES STORED HERE
-    # USEFUL FOR KEEPING ACCESS TO PRIVATE ATTRIUTES CLEAN
-    # AND NICE
-    @property
-    def name(self):
-        """
-        This property returns the name assigned to the instance
-
-        Return:
-            returns the number stored for the instance
-        """
-        return self.__name
-
-    @name.setter
-    def name(self, value):
-        """
-        This setter property gives access to re-assigning the
-        name of the instance
-
-        Args:
-            value: the new value to store for the name
-        """
-        self.__name = self._validate_value("name", value, str)
-
-    @property
-    def my_number(self):
-        """
-        This property returns the number stored for the instance
-
-        Return:
-            returns the number stored for the instance
-        """
-        return self.__my_number
-
-    @my_number.setter
-    def my_number(self, value):
-        """
-        This property gives access to re-assigning the number
-        stored for the instance
-
-        Args:
-            value: the new value to store
-        """
-        self.__my_number = self._validate_value("my_number", value, int)
-
-    @property
-    def created_at(self):
-        """
-        This property returns the datetime the instance was created
-
-        Return:
-            returns the datetime the instance was first created
-        """
-        return self.__created_at.isoformat()
-
-    @property
-    def updated_at(self):
-        """
-        This property returns the datetime the instance was created
-
-        Return:
-            returns the date the instance was updated at
-        """
-        return self.__updated_at.isoformat()
-
-    @updated_at.setter
-    def updated_at(self, value):
-        """
-        This property returns the datetime the instance was created
-
-        Args:
-            value: the value we are assigning the updated_at to
-        """
-        self.__updated_at = self._validate_value("updated_at", value, datetime)
