3c370190687503bd0f38db58c119c1c6bafdb97e
El-gibbor <chiagoziem11@yahoo.com>
2023-10-12 19:59:44 +0400

    implemented do_creat()

--- console.py
+++ console.py
@@ -26,6 +26,23 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+    def do_create(self, args):
+        """ creates a new instance, saves it (to the json file)
+        and prints the id
+        """
+        commands = args.split(" ")
+        if not commands[0]:
+            print("** class name missing **")
+        else:
+            class_name = commands[0]
+            if class_name == "BaseModel":
+                new_instance = BaseModel()
+                new_instance.save()
+                print(new_instance.id)
+            else:
+                print("** class doesn't exist **")
+            # TODO: are spaces handled as missing or non-existing arg
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
