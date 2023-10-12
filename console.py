ae3acf1cec7e5bfb0270f6145eb3c167393512b2
El-gibbor <chiagoziem11@yahoo.com>
2023-10-12 12:01:39 +0400

    defined an entry point of the cmd interpreter

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This is a command interpreter we can use to
+manipulate the ojects of our web application
+"""
+import cmd
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Defines a class which is the entry point of our command interpreter."""
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """ overides default emptyline execution """
+        pass
+
+    def do_quit(self, args):
+        """ Exits the Program
+        """
+        return True
+
+    def do_EOF(self, args):
+        """ Exits the program with EOF command like cntrl+D
+        """
+        print()
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
