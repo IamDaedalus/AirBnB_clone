e348d787a551dbd189ffbdb3afb91fd815b27edc
El-gibbor <chiagoziem11@yahoo.com>
2023-10-15 02:22:19 +0400

    unit test for base model an its class methods

--- tests/test_models/test_base_mode.py
+++ tests/test_models/test_base_mode.py
@@ -1,44 +1,70 @@
 #!/usr/bin/python3
-
-from datetime import datetime
+import uuid
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
+    """ Test case for the BaseModel class in the 'models' module."""
+
     def setUp(self):
-        self.base = BaseModel()
-
-    def test_name(self):
-        self.base.name = "Roger"
-        self.assertEqual(self.base.name, "Roger")
-
-    def test_wrong_name_type_int(self):
-        with self.assertRaises(TypeError):
-            self.base.name = 55
-
-    def test_wrong_name_type_list(self):
-        with self.assertRaises(TypeError):
-            self.base.name = [0, 2, 3]
-
-    def test_my_number(self):
-        self.base.my_number = 80085
-        self.assertEqual(self.base.my_number, 80085)
-
-    def test_wrong_number_type_str(self):
-        with self.assertRaises(TypeError):
-            self.base.my_number = "80085"
-
-    def test_wrong_number_type_list(self):
-        with self.assertRaises(TypeError):
-            self.base.my_number = [8, 0, 0, 8, 5]
-
-    def test_wrong_number_type_float(self):
-        with self.assertRaises(TypeError):
-            self.base.my_number = 800.85
+        """Set up the initial state for the tests."""
+        self.obj_1 = BaseModel()
+        self.obj_2 = BaseModel()
 
     def tearDown(self):
-        del self.base
+        """Clean up resources after each test."""
+        del self.obj_1
+        del self.obj_2
+
+    def test_instantiation(self):
+        """Test if BaseModel objects are instantiated correctly."""
+        self.assertIsInstance(self.obj_1, BaseModel)
+
+    def test_attr_type(self):
+        """Test the data types of specific attributes of BaseModel objects."""
+        with self.subTest():
+            self.assertIsInstance(self.obj_1.id, str)
+        with self.subTest():
+            self.assertIsInstance(self.obj_1.updated_at, datetime)
+
+    def test_unique_id(self):
+        """ Test if the id attribute of different
+        BaseModel instances is unique.
+        """
+        self.assertNotEqual(self.obj_1.id, self.obj_2.id)
+
+    def test_id_assignment(self):
+        """ validate that The id attribute of the BaseModel object is a
+        valid UUID.
+        """
+        self.assertTrue(uuid.UUID(self.obj_1.id, version=4))
+
+    def test_to_dict_obj(self):
+        """ Test the conversion of BaseModel object attributes to a
+        dictionary using to_dict() method.
+        """
+        self.obj_1.updated_at = datetime.utcnow()
+        obj_dict = self.obj_1.to_dict()
+
+        # Validate data types of dictionary values after to_dict() conversion
+        self.assertIsInstance(obj_dict, dict)
+        self.assertIsInstance(obj_dict["id"], str)
+        self.assertIsInstance(obj_dict["__class__"], str)
+        self.assertIsInstance(obj_dict["created_at"], str)
+        self.assertIsInstance(obj_dict["updated_at"], str)
+        self.assertEqual(obj_dict["__class__"], "BaseModel")
+
+    def test_save_method(self):
+        """ Test the save() method of the BaseModel class.
+        Asserts that The 'updated_at' attribute before calling save() is
+        not the same after calling save().
+        """
+        before_save = self.obj_1.updated_at
+        after_save = self.obj_1.save()
+        # Validate that 'updated_at' attribute is updated after calling save()
+        self.assertNotEqual(before_save, after_save)
 
 
 if __name__ == '__main__':
