90fc12c9b8d06cd6a78966f414f4a275262fde18
El-gibbor <chiagoziem11@yahoo.com>
2023-10-14 20:50:57 +0400

    a user class to handle new user objects

---
+++ models/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module holds the user class implementation, a subclass
+of BaseModel
+"""
+from base_model import BaseModel
+
+
+class User:
+    """ a class that defines a new user in the application.
+    Args:
+        email (str): users email address
+        password (str): the users password
+        first_name (str): users first name
+        last_name (str): the users last name
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
