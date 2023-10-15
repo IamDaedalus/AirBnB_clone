9d4ae2b36825353a323410c64a2b16d088318a58
El-gibbor <chiagoziem11@yahoo.com>
2023-10-15 10:40:32 +0400

    testing amenity class

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,22 +8,16 @@ class TestAmenityClass(unittest.TestCase):
     """
     Test case class for the Amenity class in the models.amenity module.
     """
-
     def test_inheritance(self):
-        """
-        Test if Amenity class inherits from BaseModel.
-        """
+        """ Test if Amenity class inherits from BaseModel. """
         self.assertTrue(issubclass(Amenity, BaseModel))
 
     def test_name_attribute(self):
-        """
-        Test if name attribute is present in the Amenity class.
-        """
+        """ Test if name attribute is present in the Amenity class."""
         self.assertTrue(hasattr(Amenity, 'name'))
 
     def test_to_dict_method(self):
-        """
-        Test if the to_dict() method of Amenity returns the
+        """ Test if the to_dict() method of Amenity returns the
         expected dictionary.
         """
         amenity = Amenity()
@@ -36,26 +30,20 @@ class TestAmenityClass(unittest.TestCase):
             'name': 'Pool',
             '__class__': 'Amenity'
         }
-
         self.assertEqual(amenity_dict, expected_dict)
 
     def test_name_attribute_type(self):
-        """
-        Test if name attribute in Amenity is a string.
-        """
+        """ Test if name attribute in Amenity is a string."""
         self.assertIsInstance(Amenity.name, str)
 
     def test_instance_creation(self):
-        """
-        Test if Amenity object is successfully created.
-        """
+        """ Test if Amenity object is successfully created."""
         amenity = Amenity()
         self.assertIsInstance(amenity, Amenity)
 
     def test_name_assignment(self):
-        """
-        Test if assigning a value to name attribute of Amenity updates
-        the attribute correctly.
+        """ Test if assigning a value to name attribute of Amenity
+        updates the attribute correctly.
         """
         amenity = Amenity()
         amenity.name = "WiFi"
